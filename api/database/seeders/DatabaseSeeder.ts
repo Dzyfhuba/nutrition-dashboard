e0de2797b2c3711bf7016bf3665809776f5d2f71
Dzyfhuba <hafidz21ub@gmail.com>
2022-11-07 07:32:35 +0700

    fix adonis es modules

--- api/database/seeders/DatabaseSeeder.ts
+++
@@ -1,9 +0,0 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import NutritionFactory from 'Database/factories/NutritionFactory'
-
-export default class extends BaseSeeder {
-  public async run() {
-    // Write your database queries inside the run method
-    await NutritionFactory.createMany(100)
-  }
-}
