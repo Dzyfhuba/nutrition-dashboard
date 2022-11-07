9d928a0693012b6b1fcd9cc28be96a941c59332c
Dzyfhuba <hafidz21ub@gmail.com>
2022-11-07 11:47:55 +0700

    add person/people resource controller

--- api/app/Controllers/Http/PeopleController.ts
+++ api/app/Controllers/Http/PeopleController.ts
@@ -1,17 +1,64 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Person from 'App/Models/Person'
 
 export default class PeopleController {
-  public async index({}: HttpContextContract) {}
+  public async index({ response }: HttpContextContract) {
+    try {
+      const people = await Person.all()
 
-  public async create({}: HttpContextContract) {}
+      return response.ok(people)
+    } catch (error) {
+      return response.internalServerError(error)
+    }
+  }
 
-  public async store({}: HttpContextContract) {}
+  public async create({ response }: HttpContextContract) {
+    return response.notFound()
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async store({ response, request }: HttpContextContract) {
+    try {
+      const person = await Person.create(request.body())
 
-  public async edit({}: HttpContextContract) {}
+      return response.created(person)
+    } catch (error) {
+      return response.internalServerError(error)
+    }
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async show({ request, response }: HttpContextContract) {
+    try {
+      const person = await Person.findOrFail(request.param('id'))
 
-  public async destroy({}: HttpContextContract) {}
+      return response.ok(person)
+    } catch (error) {
+      return response.internalServerError(error)
+    }
+  }
+
+  public async edit({ response }: HttpContextContract) {
+    return response.notFound()
+  }
+
+  public async update({ request, response }: HttpContextContract) {
+    try {
+      const person = await Person.updateOrCreate(request.body(), request.body())
+
+      return response.created(person)
+    } catch (error) {
+      return response.internalServerError(error)
+    }
+  }
+
+  public async destroy({ response, request }: HttpContextContract) {
+    try {
+      const person = await Person.findOrFail(request.param('id'))
+
+      await person.delete()
+
+      return response.ok(person)
+    } catch (error) {
+      return response.internalServerError(error)
+    }
+  }
 }
