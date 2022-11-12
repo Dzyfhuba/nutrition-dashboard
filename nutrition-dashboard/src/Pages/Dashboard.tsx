6cf12932ac2e82e5fbd97c8fd12fb4ac91035d8e
Dzyfhuba <hafidz21ub@gmail.com>
2022-11-12 18:21:47 +0700

    add filter function

--- nutrition-dashboard/src/Pages/Dashboard.tsx
+++ nutrition-dashboard/src/Pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { IoMdSearch } from 'react-icons/io'
+import { IoMdCloseCircle, IoMdSearch } from 'react-icons/io'
 import { useProSidebar } from 'react-pro-sidebar'
 import Button from '../Components/Button'
 import Input from '../Components/Input'
@@ -21,7 +21,16 @@ const Dashboard = (props: Props) => {
             <GiHamburgerMenu className='text-2xl' />
         </Button>
         <form className="flex" onSubmit={(e:React.SyntheticEvent) => {e.preventDefault();setFilter(searchString)}}>
-            <Input placeholder='Search for...' onChange={(e:React.ChangeEvent<HTMLInputElement>) => setSearchString(e.target.value.toLowerCase())} />
+            <div className="relative">
+              <Input placeholder='Search for...' onChange={(e:React.ChangeEvent<HTMLInputElement>) => setSearchString(e.target.value.toLowerCase().replace(' ', ''))} />
+              <Button 
+                type='reset' 
+                className={`bg-transparent text-blue-300 border-none px-0 py-0 aspect-square h-full hover:bg-transparent absolute right-0 ${!(filter || searchString) && 'hidden'}`}
+                onClick={() => {setFilter('');setSearchString('')}}
+              >
+                <IoMdCloseCircle className='text-2xl mx-auto' />
+              </Button>
+            </div>
             <Button className='rounded-r-md -translate-x-1' type='submit'>
                 <IoMdSearch className='text-2xl' />
             </Button>
